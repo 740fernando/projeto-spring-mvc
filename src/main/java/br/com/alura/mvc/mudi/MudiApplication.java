d82327345e6da4110f6f83df73691581f68ff370
Fernando Luiz <fernando.souzavieira@everis.nttdata.com>
2021-10-15 15:07:29 -0300

    Anotacoes sobre aprendzado na MudiApplication

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -19,3 +19,15 @@ public class MudiApplication {
 // E aconteceu o que é esperado, ele vai no banco de dados e faz o select ? NÃO
 //uando eu volto para a página e faço uma atualização da página home, várias vezes, era para termos
 // várias consultas ao banco, mas não. Ele cacheou aquele conteúdo.
+
+/**
+ * Qual é a vantagem e quando aplicar o cache na nossa aplicação?
+ * Utilizar cache neste cenário pode não apenas reduzir o custo financeiro
+ * da aplicação, como liberar capacidade de processamento para outras funcionalidades
+ * possivelmente mais importantes.
+ *
+ * Separar a página pública (sem login) das páginas restritas
+ * Redirecionar após logout para a página home
+ * Trabalhar com paginação e ordenação, usando as classes Pageable e Sort
+ * Usar o cache na aplicação para melhorar o desempenho usando a anotação @Cacheable
+ */
